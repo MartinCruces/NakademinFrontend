923596a9c5210a65d7ea4fab7537e3bfa19c7d5d
Martin Cruces <martin76e@yahoo.com>
2023-03-18 13:18:20 +0100

    vaper

---
+++ Lektion1/Övningar/functions.js
@@ -0,0 +1,8 @@
+function showDate(){
+    const dayz = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag"]
+    const months = ["januari", "februari", "mars", "april", "maj", "juni", "juli", "augusti", "september", "oktober","november", "december"]
+
+    let date = new Date().getDate(1)
+    
+
+}
